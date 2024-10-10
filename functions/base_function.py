d76abd02eb5439836ab1137994325a3cc49291c7
cezar06 <tudor.cezar01@gmail.com>
2024-10-10 17:32:28 +0300

    base function

---
+++ functions/base_function.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class BaseFunction:
+    def __init__(self, f, x_lower, x_upper):
+        self.f = f
+        self.x_lower = np.array(x_lower)
+        self.x_upper = np.array(x_upper)
+    
+    def evaluate(self, x):
+        x = np.array(x)
+        
+        # Apply control mechanisms
+        x_controlled = np.clip(x, self.x_lower, self.x_upper)
+        
+        # Evaluate and return f(x)
+        return self.f(x_controlled)
