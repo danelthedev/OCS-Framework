372e29ccfe61c5b883b7ecb3a7780aeebd3aa31e
Daniel Imbrea <daniel.imbrea2@student.tuiasi.ro>
2024-11-07 16:26:05 +0200

    base class for cga

---
+++ genetic_algorithms/cga.py
@@ -0,0 +1,25 @@
+class CGA:
+
+    def __init__(self, pc=0.8, pm=0.05, nfe=1000):
+        self.pc = pc
+        self.pm = pm
+        self.nfe = nfe
+        pass
+
+    def initialization():
+        pass
+
+    def fitness():
+        pass
+
+    def selection():
+        pass
+
+    def crossover():
+        pass
+
+    def mutation():
+        pass
+
+    def replacement():
+        pass
